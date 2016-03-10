527e3a6c032648113cf38f06af0d4612f8de9ac5
Mitchell Gates <md.fragala@gmail.com>
2016-03-10 17:31:13 -0500

    Setup gulp tasks.

--- __config__/gulpfile.js
+++ __config__/gulpfile.js
@@ -1,4 +1,40 @@
 const lazy = require('lazy-req')(require);
 const dreq = require('require-dir');
-const gulp = lazy('gulp');
+const gulp = require('gulp');
+
 const Primiere = require('./');
+const $ = Primiere.configs;
+const _ = Primiere.options;
+
+/**
+ * Pages
+ */
+gulp.task('pages', function() {});
+/**
+ * Styles
+ */
+gulp.task('styles', function() {});
+/**
+ * Scripts
+ */
+gulp.task('scripts', function() {});
+/**
+ * Images
+ */
+gulp.task('images', function() {});
+/**
+ * Assets
+ */
+gulp.task('assets', function() {});
+/**
+ * Build
+ */
+gulp.task('build', function() {});
+/**
+ * Serve
+ */
+gulp.task('serve', function() {});
+/**
+ * Watch
+ */
+gulp.task('watch', function() {});
